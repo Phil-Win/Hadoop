6abc0861c2710c0050335ca3ee42747c0aadcc09
himynameisfil <nguye208@gmail.com>
2017-02-25 11:12:28 -0500

    Fixed deploy bug

--- BuildScript/build.sh
+++ BuildScript/build.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 cd /e/deploy/
 cd extract
+sudo rm -R /e/deploy/extract/*
 
 echo "Please type in the Git Hub branch to deploy"
 read branch
 
 git clone $branch
 
-repositoryList=$(ls -d *)
+repositoryList=($(ls -d *))
 echo "Choose a repository:"
-for i in "${#repositoryList[@]}"; do
-	echo "[$i]	${repositoryList[$(($i-1))]}"
+for i in "${!repositoryList[@]}"; do
+	echo "[$(($i+1))]	${repositoryList[$i]}"
 done
 
 read choice
 
-repo="${repositoryList[$(($i-1))]}"
-
+repo="${repositoryList[$(($choice-1))]}"
+echo "Chosen the repo $repo"
 cd "$repo"
 
-projectList=$(ls -d *)
+projectList=($(ls -d *))
 echo "Choose a project:"
-for i in "${#projectList[@]}"; do
-	echo "[$i]	${projectList[$(($i-1))]}"
+for i in "${!projectList[@]}"; do
+	echo "[$(($i+1))]	${projectList[$i]}"
 done
 
 read choice
 
-project="${projectList[$(($i-1))]}"
-
+project="${projectList[$(($choice-1))]}"
+echo "Chosen the project $project"
 cd "$project"
 
-workflowList=$(ls -d *)
+workflowList=($(ls -d *))
 echo "Choose a workflow:"
-for i in "${#workflowList[@]}"; do
-	echo "[$i]	${workflowList[$(($i-1))]}"
+for i in "${!workflowList[@]}"; do
+	echo "[$(($i+1))]	${workflowList[$i]}"
 done
 
 read choice
 
-workflow="${workflowList[$(($i-1))]}"
+workflow="${workflowList[$(($choice-1))]}"
+echo "Chosen the workflow $workflow"
+
 cd /e/deploy/extract
 
 cp $repo/$project/$workflow/Properties/* /e/project/${repo}/dynamicProperties
